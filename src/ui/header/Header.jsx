1a3f8d49b50ef73e5616269e2ffb8a79737c08b8
Lord Frantex <lordfranktex@gmail.com>
2024-07-08 23:53:32 +0100

    done

--- src/ui/header/Header.jsx
+++ src/ui/header/Header.jsx
@@ -12,17 +12,15 @@ const Header = () => {
 
             <div className="container">
                 <nav>
-                    <ul className={toggle && 'responsive'}>
-                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Mide Chair</a></li>
-                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Seun Chair</a></li>
-                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Fae Chair</a></li>
-                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Zoe Chair</a></li>
-                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Discounted Products</a></li>
-                    </ul>
                     <div className="logo">
                         <span onClick={() => setToggle(prev => !prev)}><FaBars /></span>
                         <h2>VEECKER</h2>
                     </div>
+                    <ul className={toggle && 'responsive'}>
+                        <li><a href="/" onClick={() => setToggle(prev => !prev)}>Home</a></li>
+                        <li><a href="#" onClick={() => setToggle(prev => !prev)}>Discounted Products</a></li>
+                    </ul>
+
                     <div className="right-section">
                         <Link href="/cart"><FaShoppingCart /></Link>
                         <Link href="/cart" className='user'><FaUser /></Link>
